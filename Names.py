ef93c67d903f8c0c166ef643089c830a01d5084d
Alyssa <165924583+AlyMad@users.noreply.github.com>
2024-04-19 07:53:29 -0700

    Add files via upload

---
+++ Names.py
@@ -0,0 +1,10 @@
+first = input("What is your first name?")
+first = first[0]
+
+middle = input("What is your middle name?")
+middle = middle[0]
+
+last = input("What is your lastname?")
+last = last[0]
+
+print(first, middle, last)
